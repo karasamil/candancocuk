6c51060d136692f8b862be1a56d1bdb38ba49c2b
karasamil <karaomersamil@gmail.com>
2021-01-06 13:26:46 +0300

    Warheads added...

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,19 @@
-import React from 'react';
+import React, {useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [count, setCount] = useState<number>(0);
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          {count} nuclear warheads have been launched!!!
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button onClick={() => {setCount(count + 1)}}>
+          Launch Warhead
+        </button>
       </header>
     </div>
   );
